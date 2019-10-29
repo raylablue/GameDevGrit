9cc2bbd2b2feb91587c2530f4f81aa9842047442
Rachel Blue <rachel17.22@gmail.com>
2019-10-28 19:34:17 -0600

    Call to action button and layout

--- src/components/organisms/CallToAction/CallToAction.js
+++ src/components/organisms/CallToAction/CallToAction.js
@@ -1,13 +1,37 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDiscord} from "@fortawesome/free-brands-svg-icons";
 
 function CallToAction() {
     return (
         <div className="container">
-            <p className="justify-content-center">
-                This is a test paragraph to talk about what this comminity is an urge you to join the discord channel.
-            </p>
+            <div className="wrapper-cta">
+
+                <div className="row">
+                    <div className="col">
+                        <div className="d-flex justify-content-end">
+                            <button type="button" className="btn btn-primary-3">
+                                <FontAwesomeIcon className="size fa-6x text-white" icon={faDiscord} />
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="col-sm-8">
+                        <div className="justify-content-start">
+                            <h3>Join our discord server</h3>
+                            <p>
+                                Listen to the show live, chat with guests and other game developers.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
 
 export default CallToAction;
+
+
+
