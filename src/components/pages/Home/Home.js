00502e26f5e00e7707d41bc1a25486d2aa488adc
Rachel Blue <rachel17.22@gmail.com>
2019-11-08 16:57:09 -0700

    ABEM added to Home and fpo banner image replaced

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./Home.scss";
 import NavBar from "../../organisms/NavBar/NavBar";
 import EpisodeData from "../../organisms/EpisodeData/EpisodeData";
 import "../../../styles/theme.scss";
 import CallToAction from "../../organisms/CallToAction/CallToAction";
-import fpoBanner from './FPObanner.jpg';
+import Banner from './gdg-banner.png';
 
 function Home() {
     return (
         <div>
             <NavBar />
-            <img className="img-wide" alt="fpo sample banner" src={fpoBanner} />
+            <img
+                className="p-home__img-wide align-content-lg-center"
+                alt="game dev grit banner"
+                src={Banner}
+            />
             <CallToAction/>
             <EpisodeData />
 
